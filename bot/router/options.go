512dbd805aafa73d82c632ff94ab76e832540a64
Cufee <vkouzin@outlook.com>
2023-03-29 15:27:09 -0400

    deleting global commands as well when guild is set

--- bot/router/options.go
+++ bot/router/options.go
@@ -12,6 +12,16 @@ func RegisterCommandOptions(s *discordgo.Session, guildID string, cleanup bool) 
 		if err != nil {
 			return nil, err
 		}
+
+		if guildID != "" {
+			// Delete global commands as well
+			globalCommands, err := s.ApplicationCommands(s.State.User.ID, "")
+			if err != nil {
+				return nil, err
+			}
+			commands = append(commands, globalCommands...)
+		}
+
 		for _, v := range commands {
 			err := s.ApplicationCommandDelete(s.State.User.ID, guildID, v.ID)
 			if err != nil {
