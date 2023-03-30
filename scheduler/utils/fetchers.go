85401001b48674fcc1eca83dd7fbdc05651f48d5
Cufee <vkouzin@outlook.com>
2023-03-29 20:27:53 -0400

    added a browser user-agent header, mostly for Reddit

--- scheduler/utils/fetchers.go
+++ scheduler/utils/fetchers.go
@@ -14,7 +14,13 @@ func Fetch(url string) ([]byte, error) {
 		Transport: getProxyTransport(),
 	}
 
-	resp, err := client.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
